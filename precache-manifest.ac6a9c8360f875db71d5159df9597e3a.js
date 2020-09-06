a2f773b1efd49c99400073066dbc58f160a7c63d
unknown <dariush.hassani@outlook.com>
2020-09-06 21:16:27 +0430

    Updates

---
+++ precache-manifest.ac6a9c8360f875db71d5159df9597e3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92159dce2215ccfdacc9976b82442ca4",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "e54bcfc355a3798f46dd",
+    "url": "/my-web/static/css/main.e56aef3b.chunk.css"
+  },
+  {
+    "revision": "b20dd9419ac788b69997",
+    "url": "/my-web/static/js/2.4a5f3c0a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-web/static/js/2.4a5f3c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54bcfc355a3798f46dd",
+    "url": "/my-web/static/js/main.02fbdd3f.chunk.js"
+  },
+  {
+    "revision": "5b1b7f907bbee126f38d",
+    "url": "/my-web/static/js/runtime-main.4abdfd6f.js"
+  },
+  {
+    "revision": "7f302b0220a93b4e05b149137a6d1e74",
+    "url": "/my-web/static/media/Logo.7f302b02.png"
+  },
+  {
+    "revision": "798e3cbfeae079f7339f086ec98efbcf",
+    "url": "/my-web/static/media/dribbble.798e3cbf.png"
+  },
+  {
+    "revision": "c8cbe8617ca029c01f7c73efbb124e71",
+    "url": "/my-web/static/media/instagram.c8cbe861.png"
+  }
+]);
